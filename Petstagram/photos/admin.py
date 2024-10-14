86a49df64a3f5e317c8a5e104bc8fb3cd5adfad6
tsvtln <tsvtln@abv.bg>
2024-10-14 03:43:52 +0300

    feat: added a comment feature

--- Petstagram/photos/admin.py
+++ Petstagram/photos/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-
 from Petstagram.photos.models import Photo
 
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'date_of_publication',
+        'description',
+        'get_tagged_pets'
+    )
+
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join(str(pet) for pet in obj.tagged_pets.all())
