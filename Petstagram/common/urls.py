531574d245a7d23017dc1dd20d1ff2bc9b218bed
tsvtln <tsvtln@abv.bg>
2024-10-21 04:12:15 +0300

    FEAT: added like functionality

--- Petstagram/common/urls.py
+++ Petstagram/common/urls.py
@@ -4,4 +4,5 @@ from Petstagram.common import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('like/<int:photo_id>/', views.likes_functionality, name='like')
 ]
--- Petstagram/common/views.py
+++ Petstagram/common/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from Petstagram.common.models import Like
 from Petstagram.photos.models import Photo
 
 
 def home_page(request):
-
     all_photos = Photo.objects.all()
     context = {
         'all_photos': all_photos
     }
     return render(request, 'common/home-page.html', context)
 
+
+def likes_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(to_photo_id=photo_id).first()
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+
+
+# def share_functionality()
