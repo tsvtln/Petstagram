b5ada3a5c08d7099859e226f1d24b64f0f5f69ba
tsvtln <tsvtln@abv.bg>
2024-10-21 03:39:42 +0300

    fix: fixed home view, some html fixes

--- Petstagram/common/views.py
+++ Petstagram/common/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from Petstagram.photos.models import Photo
+
+
 def home_page(request):
-    return render(request, 'common/home-page.html')
+
+    all_photos = Photo.objects.all()
+    context = {
+        'all_photos': all_photos
+    }
+    return render(request, 'common/home-page.html', context)
+
