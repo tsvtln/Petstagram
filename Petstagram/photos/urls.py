ffd3ada4f227c308dca6c232874eda115e47b154
tsvtln <tsvtln@abv.bg>
2024-10-26 02:38:18 +0300

    FIX: forgot to fix the URL for details page

--- Petstagram/photos/urls.py
+++ Petstagram/photos/urls.py
@@ -4,7 +4,7 @@ from Petstagram.photos import views
 urlpatterns = [
     path('add/', views.PhotoAddPageView.as_view(), name='photo-add'),
     path('<int:pk>/', include([
-        path('', views.photo_details_page, name='photo-details'),
+        path('', views.PhotoDetailsView.as_view(), name='photo-details'),
         path('edit/', views.PhotoEditPageView.as_view(), name='photo-edit'),
         path('delete/', views.photo_delete, name='photo-delete'),
     ]))
