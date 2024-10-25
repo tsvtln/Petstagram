5bc3b634508a5035fd41e44990092d7f7778ea1e
tsvtln <tsvtln@abv.bg>
2024-10-25 23:51:51 +0300

    FIX: CBV for add_page & details page

--- Petstagram/pets/urls.py
+++ Petstagram/pets/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from Petstagram.pets import views
 
 urlpatterns = [
-    path('add/', views.pet_add_page, name='add-pet'),
+    path('add/', views.PetAddPage.as_view(), name='add-pet'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
-        path('', views.pet_details_page, name='pet-details'),
+        path('', views.PetDetailsPage.as_view(), name='pet-details'),
         path('edit/', views.pet_edit_page, name='edit-pet'),
         path('delete/', views.pet_delete_page, name='delete-pet')
     ]))
--- Petstagram/pets/views.py
+++ Petstagram/pets/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
 
 from Petstagram.common.forms import CommentForm
 from Petstagram.pets.forms import PetAddForm, PetEditForm, PetDeleteForm
 from Petstagram.pets.models import Pet
 
 
-def pet_add_page(request):
-    form = PetAddForm(request.POST or None)
+class PetAddPage(CreateView):
+    model = Pet
+    form_class = PetAddForm
+    template_name = 'pets/pet-add-page.html'
+    success_url = reverse_lazy('profile-details', kwargs={'pk': 1})
 
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('profile-details', pk=1)
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'pets/pet-add-page.html', context)
+# def pet_add_page(request):
+#     form = PetAddForm(request.POST or None)
+#
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             return redirect('profile-details', pk=1)
+#
+#     context = {
+#         'form': form,
+#     }
+#
+#     return render(request, 'pets/pet-add-page.html', context)
 
 
 def pet_delete_page(request, username: str, pet_slug: str):
@@ -53,15 +61,27 @@ def pet_edit_page(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-edit-page.html', context)
 
 
-def pet_details_page(request, username: str, pet_slug: str):
-    pet = Pet.objects.get(slug=pet_slug)
-    all_photos = pet.photo_set.all()
-    comment_form = CommentForm()
+class PetDetailsPage(DetailView):
+    model = Pet
+    template_name = 'pets/pet-details-page.html'
+    slug_url_kwarg = 'pet_slug'
 
-    context = {
-        'pet': pet,
-        'all_photos': all_photos,
-        'comment_form': comment_form,
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_photos'] = context['pet'].photo_set.all()
+        context['comment_form'] = CommentForm()
+        return context
 
-    return render(request, 'pets/pet-details-page.html', context)
+
+# def pet_details_page(request, username: str, pet_slug: str):
+#     pet = Pet.objects.get(slug=pet_slug)
+#     all_photos = pet.photo_set.all()
+#     comment_form = CommentForm()
+#
+#     context = {
+#         'pet': pet,
+#         'all_photos': all_photos,
+#         'comment_form': comment_form,
+#     }
+#
+#     return render(request, 'pets/pet-details-page.html', context)
